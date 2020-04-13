272e7192e8db250d62dc7b32f1dc98903e85f8b0
Igor Nascimento <igorns@motorola.com>
2020-04-13 20:25:56 -0300

    Adding InfoLogActivity

--- AndroidApp/app/src/main/java/com/example/androidapp/InfoLogActivity.java
+++ AndroidApp/app/src/main/java/com/example/androidapp/InfoLogActivity.java
@@ -1,14 +1,13 @@
 package com.example.androidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class InfoLogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.info_log);
+        setContentView(R.layout.activity_main);
     }
 }
